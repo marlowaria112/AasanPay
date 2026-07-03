c3e9799d34527eb79d11425f5f4f2989da825f6b
marlowaria112 <marlowaria112@gmail.com>
2026-07-03 21:44:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ const db = initializeFirestore(firebaseApp, {
   experimentalAutoDetectLongPolling: true,
 });
 const auth = getAuth(firebaseApp);
-const auth = getAuth(firebaseApp);
 
 const FONT = `@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap');`;
 
